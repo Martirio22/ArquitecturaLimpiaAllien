de2e43e77fe33e328849999859adbafcf9e245ad
CHRISTIAN A. GALLEGOS YAGUACHI <christian.gallegos@inventar-tech.com>
2025-12-17 09:58:17 -0500

    I en la interfaces

--- src/main/java/com/aliengnss/backend/infraestructura/repositorios/IUsuarioJpaRepository.java
+++ src/main/java/com/aliengnss/backend/infraestructura/repositorios/IUsuarioJpaRepository.java
@@ -1,7 +1,6 @@
 package com.aliengnss.backend.infraestructura.repositorios;
 
 
-import com.aliengnss.backend.dominio.entidades.Usuario;
 import com.aliengnss.backend.infraestructura.persistencia.jpa.UsuarioJpa;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface UsuarioJpaRepository extends JpaRepository<UsuarioJpa, Long> {
+public interface IUsuarioJpaRepository extends JpaRepository<UsuarioJpa, Long> {
 
     Optional<UsuarioJpa> findByNombreUsuario(String nombreUsuario);
 
