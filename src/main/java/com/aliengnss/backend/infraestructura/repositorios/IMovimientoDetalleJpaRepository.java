fcf10c760b33c3a5733231c10e089fde51f51c59
SaancA95 <158068443+SaancA95@users.noreply.github.com>
2025-12-18 09:30:41 -0500

    Añadiendo repositorios

---
+++ src/main/java/com/aliengnss/backend/infraestructura/repositorios/IMovimientoDetalleJpaRepository.java
@@ -0,0 +1,9 @@
+package com.aliengnss.backend.infraestructura.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aliengnss.backend.infraestructura.persistencia.jpa.MovimientoDetalleJpa;
+
+public interface IMovimientoDetalleJpaRepository extends JpaRepository<MovimientoDetalleJpa, Long> {
+
+}
