ca4c60ca842bc590d2905fab9bdba80c40cf7df6
Christian Gallegos <christian@dev.com>
2025-12-18 18:55:12 -0800

    Repository duplicado eliminado

--- src/main/java/com/aliengnss/backend/infraestructura/repositorios/CompraProductoDetalleJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aliengnss.backend.infraestructura.repositorios;
-
-import com.aliengnss.backend.infraestructura.persistencia.jpa.CompraProductoDetalleJpa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompraProductoDetalleJpaRepository extends JpaRepository<CompraProductoDetalleJpa,Long> {
-}
