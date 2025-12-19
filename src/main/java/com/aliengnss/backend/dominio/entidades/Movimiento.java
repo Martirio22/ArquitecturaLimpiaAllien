6ed6d0d3c5efc0a0c633e044378d5775fcb0b196
Martin Guerron <guerronmartin@gmail.com>
2025-12-18 23:28:55 -0500

    mejorando entidades de date local date

--- src/main/java/com/aliengnss/backend/dominio/entidades/Movimiento.java
+++ src/main/java/com/aliengnss/backend/dominio/entidades/Movimiento.java
@@ -1,65 +1,67 @@
 package com.aliengnss.backend.dominio.entidades;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.time.LocalDateTime;
 
 public class Movimiento implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private final Long idMovimiento;
-	private final int idCompraProducto;
-	private final Date fechaMovimiento;
-	private final int idUsuario;
-	private final int idUbicacionOrigen;
-	private final int idUbicacionDestino;
-	private final String tipo;
-	private final String observaciones;
+    private final Long idMovimiento;
+    private final int idCompraProducto;
+    private final LocalDateTime fechaMovimiento;
+    private final int idUsuario;
+    private final int idUbicacionOrigen;
+    private final int idUbicacionDestino;
+    private final String tipo;
+    private final String observaciones;
 
-	
+    public Movimiento(Long idMovimiento,
+                      int idCompraProducto,
+                      LocalDateTime fechaMovimiento,
+                      int idUsuario,
+                      int idUbicacionOrigen,
+                      int idUbicacionDestino,
+                      String tipo,
+                      String observaciones) {
+        this.idMovimiento = idMovimiento;
+        this.idCompraProducto = idCompraProducto;
+        this.fechaMovimiento = fechaMovimiento;
+        this.idUsuario = idUsuario;
+        this.idUbicacionOrigen = idUbicacionOrigen;
+        this.idUbicacionDestino = idUbicacionDestino;
+        this.tipo = tipo;
+        this.observaciones = observaciones;
+    }
 
-	private Movimiento(Long idMovimiento, int idCompraProducto, Date fechaMovimiento, int idUsuario,
-			int idUbicacionOrigen, int idUbicacionDestino, String tipo, String observaciones) {
-		super();
-		this.idMovimiento = idMovimiento;
-		this.idCompraProducto = idCompraProducto;
-		this.fechaMovimiento = fechaMovimiento;
-		this.idUsuario = idUsuario;
-		this.idUbicacionOrigen = idUbicacionOrigen;
-		this.idUbicacionDestino = idUbicacionDestino;
-		this.tipo = tipo;
-		this.observaciones = observaciones;
-	}
+    public Long getIdMovimiento() {
+        return idMovimiento;
+    }
 
-	public Long getIdUbicacion() {
-		return idMovimiento;
-	}
+    public int getIdCompraProducto() {
+        return idCompraProducto;
+    }
 
-	public int getIdCompraProducto() {
-		return idCompraProducto;
-	}
+    public LocalDateTime getFechaMovimiento() {
+        return fechaMovimiento;
+    }
 
-	public Date getFechaMovimiento() {
-		return fechaMovimiento;
-	}
+    public int getIdUsuario() {
+        return idUsuario;
+    }
 
-	public int getIdUsuario() {
-		return idUsuario;
-	}
+    public int getIdUbicacionOrigen() {
+        return idUbicacionOrigen;
+    }
 
-	public int getIdUbicacionOrigen() {
-		return idUbicacionOrigen;
-	}
+    public int getIdUbicacionDestino() {
+        return idUbicacionDestino;
+    }
 
-	public int getIdUbicacionDestino() {
-		return idUbicacionDestino;
-	}
+    public String getTipo() {
+        return tipo;
+    }
 
-	public String getTipo() {
-		return tipo;
-	}
-
-	public String getObservaciones() {
-		return observaciones;
-	}
-
+    public String getObservaciones() {
+        return observaciones;
+    }
 }
